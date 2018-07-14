787ea36c159c20ef418bce5c498b663678c00279
Ben Lipson <sonofblip@gmail.com>
2018-07-14 15:15:18 -0500

    Make line function take points as vectors instead of x and y

--- main.cpp
+++ main.cpp
@@ -9,26 +9,26 @@ Model *model = nullptr;
 const int width = 800;
 const int height = 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color)
 {
     bool steep = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y))
     {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         steep = true;
     }
-    if (x0 > x1)
+    if (p0.x > p1.x)
     {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
     }
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
     int derror2 = std::abs(dy) * 2;
     int error2 = 0;
-    int y = y0;
-    for (int x = x0; x <= x1; x++)
+    int y = p0.y;
+    for (int x = p0.x; x <= p1.x; x++)
     {
         if (steep)
         {
@@ -40,13 +40,19 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
         error2 += derror2;
         if (error2 > dx)
         {
-            y += (y1 > y0 ? 1 : -1);
+            y += (p1.y > p0.y ? 1 : -1);
             error2 -= dx * 2;
         }
     }
 }
+//
+//void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+//{
+//    line(t0, t1, image, color);
+//    line()
+//}
 
-int main(int argc, char **argv)
+int drawWireframe(int argc, char **argv)
 {
     if (argc == 2)
     {
@@ -64,11 +70,13 @@ int main(int argc, char **argv)
         {
             Vec3f v0 = model->vert(face[j]);
             Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            int x0 = (v0.x + 1.) * width / 2.;
-            int y0 = (v0.y + 1.) * height / 2.;
-            int x1 = (v1.x + 1.) * width / 2.;
-            int y1 = (v1.y + 1.) * height / 2.;
-            line(x0, y0, x1, y1, image, white);
+            Vec2i p0;
+            Vec2i p1;
+            p0.x = static_cast<int>((v0.x + 1.) * width / 2.);
+            p0.y = static_cast<int>((v0.y + 1.) * height / 2.);
+            p1.x = static_cast<int>((v1.x + 1.) * width / 2.);
+            p1.y = static_cast<int>((v1.y + 1.) * height / 2.);
+            line(p0, p1, image, white);
         }
     }
 
@@ -77,4 +85,9 @@ int main(int argc, char **argv)
     image.write_tga_file("output.tga");
     delete model;
     return 0;
+}
+
+int main(int argc, char**argv)
+{
+    return drawWireframe(argc, argv);
 }
