766504fad01cf8fb1265d5b6203f58c896d06c5e
Ben Lipson <sonofblip@gmail.com>
2018-07-14 15:45:28 -0500

    Fill in 2D triangles

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model *model = nullptr;
 const int width = 800;
 const int height = 800;
@@ -45,12 +46,44 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color)
         }
     }
 }
-//
-//void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
-//{
-//    line(t0, t1, image, color);
-//    line()
-//}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+{
+    if (t0.y == t1.y && t0.y == t2.y)
+    {
+        return;
+    }
+    if (t0.y > t1.y)
+    {
+        std::swap(t0, t1);
+    }
+    if (t0.y > t2.y)
+    {
+        std::swap(t0, t2);
+    }
+    if (t1.y > t2.y)
+    {
+        std::swap(t1, t2);
+    }
+    int totalHeight = t2.y - t0.y;
+    for (int i = 0; i < totalHeight; i++)
+    {
+        bool upperHalf = i > t1.y - t0.y || t1.y == t0.y;
+        int segmentHeight = upperHalf ? t2.y - t1.y + 1 : t1.y - t0.y + 1;
+        float alpha = (float) i / totalHeight;
+        float beta = (float) (i - (upperHalf ? t1.y - t0.y : 0)) / segmentHeight;
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = upperHalf ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        if (A.x>B.x)
+        {
+            std::swap(A, B);
+        }
+        for (int j = A.x; j <= B.x; j++)
+        {
+            image.set(j, t0.y + i, color);
+        }
+    }
+}
 
 int drawWireframe(int argc, char **argv)
 {
@@ -89,5 +122,14 @@ int drawWireframe(int argc, char **argv)
 
 int main(int argc, char**argv)
 {
-    return drawWireframe(argc, argv);
+    TGAImage image(width, height, TGAImage::RGB);
+    Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
+    image.flip_vertically();
+    image.write_tga_file("output.tga");
+    return 0;
 }
