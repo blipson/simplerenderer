532bc5f04d0cb19ada8dd6675e1712d1b1e202ad
Z003YW4 <ben.lipson@target.com>
2020-02-26 15:49:12 -0600

    remove clang errors

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void line(int x0, int y0, int x1, int y1, TgaImage &image, const TgaColor &color
     int dx = x1 - x0;
     int dy = y1 - y0;
     float m = (float) dy / (float) dx;
-    float b = y1 - (m * x1);
+    float b = float(y1) - (m * float(x1));
 
     int minX = std::min(x0, x1);
     int maxX = std::max(x0, x1);
@@ -21,7 +21,7 @@ void line(int x0, int y0, int x1, int y1, TgaImage &image, const TgaColor &color
         int maxY = std::max(y0, y1);
         for (int y = minY; y <= maxY; y++)
         {
-            int x = (y - b) / m;
+            int x = int(std::round((float(y) - b) / m));
             image.set(x, y, color);
         }
     }
@@ -29,7 +29,7 @@ void line(int x0, int y0, int x1, int y1, TgaImage &image, const TgaColor &color
     {
         for (int x = minX; x <= maxX; x++)
         {
-            int y = (m * x) + b;
+            int y = int(std::round((m * float(x)) + b));
             image.set(x, y, color);
         }
     }
@@ -39,23 +39,28 @@ void digitalDifferentialAnalyzerLine(int x0, int y0, int x1, int y1, TgaImage &i
 {
     float x, y, dx, dy, step, i;
 
-    dx = (x1 - x0);
-    dy = (y1 - y0);
-    if (abs(dx) >= abs(dy))
+    dx = float(x1 - x0);
+    dy = float(y1 - y0);
+
+    float absDx = abs(dx);
+    float absDy = abs(dy);
+
+    if (absDx >= absDy)
     {
-        step = abs(dx);
+        step = absDx;
     }
     else
     {
-        step = abs(dy);
+        step = absDy;
     }
     dx = dx / step;
     dy = dy / step;
-    x = x0;
-    y = y0;
+    x = float(x0);
+    y = float(y0);
     i = 1;
-    while (i <= step) {
-        image.set(x, y, color);
+    while (i <= step)
+    {
+        image.set(int(std::round(x)), int(std::round(y)), color);
         x = x + dx;
         y = y + dy;
         i = i + 1;
@@ -65,50 +70,53 @@ void digitalDifferentialAnalyzerLine(int x0, int y0, int x1, int y1, TgaImage &i
 void bresenhamLine(int x0, int y0, int x1, int y1, TgaImage &image, const TgaColor &color)
 {
     bool steep = false;
-    if (std::abs(x0-x1)<std::abs(y0-y1)) {
+    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    {
         std::swap(x0, y0);
         std::swap(x1, y1);
         steep = true;
     }
-    if (x0>x1) {
+    if (x0 > x1)
+    {
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
-    int dx = x1-x0;
-    int dy = y1-y0;
-    int derror2 = std::abs(dy)*2;
-    int error2 = 0;
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int dError = std::abs(dy) * 2;
+    int error = 0;
     int y = y0;
-    for (int x=x0; x<=x1; x++) {
-        if (steep) {
+    for (int x = x0; x <= x1; x++)
+    {
+        if (steep)
+        {
             image.set(y, x, color);
-        } else {
+        }
+        else
+        {
             image.set(x, y, color);
         }
-        error2 += derror2;
-        if (error2 > dx) {
-            y += (y1>y0?1:-1);
-            error2 -= dx*2;
+        error += dError;
+        if (error > dx)
+        {
+            y += (y1 > y0 ? 1 : -1);
+            error -= dx * 2;
         }
     }
 }
 
-int iPartOfNumber(float x)
-{
-    return (int)x;
-}
-
-//rounds off a number
-int roundNumber(float x)
-{
-    return iPartOfNumber(x + 0.5) ;
-}
 
 //returns fractional part of a number
 float fPartOfNumber(float x)
 {
-    if (x>0) return x - iPartOfNumber(x);
-    else return x - (iPartOfNumber(x)+1);
+    if (x > 0)
+    {
+        return x - std::round(x);
+    }
+    else
+    {
+        return x - std::round(x) + 1;
+    }
 
 }
 
@@ -118,75 +126,101 @@ float rfPartOfNumber(float x)
 }
 
 
-void xiaolinWuLine(int x0 , int y0 , int x1 , int y1, TgaImage &image, const TgaColor &color)
+void xiaolinWuLine(int x0, int y0, int x1, int y1, TgaImage &image, const TgaColor &color)
 {
-    int steep = std::abs(y1 - y0) > std::abs(x1 - x0) ;
+    int steep = std::abs(y1 - y0) > std::abs(x1 - x0);
 
     // swap the co-ordinates if slope > 1 or we
     // draw backwards
     if (steep)
     {
-        std::swap(x0 , y0);
-        std::swap(x1 , y1);
+        std::swap(x0, y0);
+        std::swap(x1, y1);
     }
     if (x0 > x1)
     {
-        std::swap(x0 ,x1);
-        std::swap(y0 ,y1);
+        std::swap(x0, x1);
+        std::swap(y0, y1);
     }
 
     //compute the slope
-    float dx = x1-x0;
-    float dy = y1-y0;
-    float gradient = dy/dx;
+    auto dx = float(x1 - x0);
+    auto dy = float(y1 - y0);
+    float gradient = dy / dx;
     if (dx == 0.0)
+    {
         gradient = 1;
+    }
 
     int xpxl1 = x0;
     int xpxl2 = x1;
-    float intersectY = y0;
+    auto intersectY = float(y0);
 
     // main loop
     if (steep)
     {
-        for (int x = xpxl1 ; x <=xpxl2 ; x += 1)
+        for (int x = xpxl1; x <= xpxl2; x += 1)
         {
             // pixel coverage is determined by fractional
             // part of y co-ordinate
             float firstCoef = rfPartOfNumber(intersectY);
             std::cout << firstCoef << std::endl;
-            image.set(iPartOfNumber(intersectY), x,
-                      TgaColor(color.r * firstCoef, color.g * firstCoef, color.b * firstCoef, 255));
+            image.set(int(std::round(intersectY)), x,
+                      TgaColor(
+                              char(float(color.r) * firstCoef),
+                              char(float(color.g) * firstCoef),
+                              char(float(color.b) * firstCoef),
+                              255
+                      )
+            );
             float secondCoef = fPartOfNumber(intersectY);
             std::cout << secondCoef << std::endl;
-            image.set(iPartOfNumber(intersectY)-1, x,
-                      TgaColor(color.r * secondCoef, color.g * secondCoef, color.b * secondCoef, 255));
+            image.set(int(std::round(intersectY)) - 1, x,
+                      TgaColor(
+                              char(float(color.r) * secondCoef),
+                              char(float(color.g) * secondCoef),
+                              char(float(color.b) * secondCoef),
+                              255
+                      )
+            );
             intersectY += gradient;
         }
     }
     else
     {
-        for (int x = xpxl1 ; x <=xpxl2 ; x += 1)
+        for (int x = xpxl1; x <= xpxl2; x += 1)
         {
             // pixel coverage is determined by fractional
             // part of y co-ordinate
             float firstCoef = rfPartOfNumber(intersectY);
             std::cout << firstCoef << std::endl;
-            image.set(x, iPartOfNumber(intersectY),
-                      TgaColor(color.r * firstCoef, color.g * firstCoef, color.b * firstCoef, 255));
+            image.set(x, int(std::round(intersectY)),
+                      TgaColor(
+                              char(float(color.r) * firstCoef),
+                              char(float(color.g) * firstCoef),
+                              char(float(color.b) * firstCoef),
+                              255
+                      )
+            );
             float secondCoef = fPartOfNumber(intersectY);
             std::cout << secondCoef << std::endl;
-            image.set(x, iPartOfNumber(intersectY)-1,
-                      TgaColor(color.r * secondCoef, color.g * secondCoef, color.b * secondCoef, 255));
+            image.set(x, int(std::round(intersectY)) - 1,
+                      TgaColor(
+                              char(float(color.r) * secondCoef),
+                              char(float(color.g) * secondCoef),
+                              char(float(color.b) * secondCoef),
+                              255
+                      )
+            );
             intersectY += gradient;
         }
     }
 
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    auto *model = new Model("/Users/z003yw4/Dev/simplerenderer/targetitem.obj");
+    auto *model = new Model("/Users/z003yw4/Dev/simplerenderer/input/head.obj");
     const int width = 500;
     const int height = 500;
 
@@ -198,16 +232,18 @@ int main(int argc, char** argv)
         {
             Vec3f v0 = model->vert(face[j]);
             Vec3f v1 = model->vert(face[(j + 1) % 3]); // use the modulo to go back to 0 and connect the triangle
-            int x0 = (v0.x + 17) * width / 35;
-            int y0 = (v0.y + 2) * height / 35;
-            int x1 = (v1.x + 17) * width / 35;
-            int y1 = (v1.y + 2) * height / 35;
-            line(x0, y0, x1, y1, image, white);
+
+            // these constants change based on the size of the OBJ file
+            int x0 = (v0.x + 1) * width / 2;
+            int y0 = (v0.y + 1) * height / 2;
+            int x1 = (v1.x + 1) * width / 2;
+            int y1 = (v1.y + 1) * height / 2;
+            xiaolinWuLine(x0, y0, x1, y1, image, white);
         }
     }
 
     image.flipVertically(); // i want to have the origin at the left bottom corner of the image
-    image.writeTgaFile("targetitem.tga");
+    image.writeTgaFile("output/testx.tga");
     delete model;
     return 0;
 
